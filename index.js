6ec61127ef4ad10b4d19bb31a6105ff0882b904a
Youssef Riahi <yriahi@cainc.com>
2023-05-31 16:31:37 -0400

    work in progress

---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function run() {
+  try {
+    // Get inputs from the workflow file
+    const extraPlugins = core.getInput('extra_plugins');
+    const githubToken = core.getInput('GITHUB_TOKEN');
+    const npmToken = core.getInput('NPM_TOKEN');
+
+    // Install extra plugins if specified
+    if (extraPlugins) {
+      const plugins = extraPlugins.split('\n');
+      for (const plugin of plugins) {
+        await exec.exec('npm', ['install', '--save-dev', plugin]);
+      }
+    }
+
+    // Perform semantic release actions
+    await exec.exec('npx', ['semantic-release'], {
+      env: {
+        GITHUB_TOKEN: githubToken,
+        NPM_TOKEN: npmToken,
+      },
+    });
+
+    // Set the outputs
+    core.setOutput('new_release_published', 'true');
+    // Set other outputs based on the release process
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();

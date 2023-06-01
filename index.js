8cf494f7e215b88f628fe35f6e7869cc64e16a01
Youssef Riahi <yriahi@cainc.com>
2023-05-31 23:03:18 -0400

    work in progress

--- index.js
+++ index.js
@@ -1,61 +1,19 @@
-const core = require('@actions/core');
-const { exec } = require('child_process');
+const exec = require('./src/_exec');
 const path = require('path');
 
-
-const installActionsCore = async () => {
-  const options = {
-    cwd: path.resolve(__dirname),
-    shell: true
-  };
-  await executeCommand('npm install --save @actions/core', options);
-};
-
-
 const run = async () => {
-  try {
-    // Install actions/core if it is not installed
-    if (!actionsCore) {
-      await installActionsCore();
-    }
-
-    // Install Dependencies
-    {
-      const options = {
-        cwd: path.resolve(__dirname),
-        shell: true
-      };
-      await executeCommand('npm ci --only=prod', options);
-    }
-
-    // Perform semantic release actions
-    {
-      const options = {
-        cwd: path.resolve(__dirname),
-        shell: true
-      };
-      await executeCommand('npx semantic-release', options);
-    }
-
-    // Set the outputs
-    core.setOutput('new_release_published', 'true');
-    // Set other outputs based on the release process
-
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-};
-
-const executeCommand = async (command, options) => {
-  return new Promise((resolve, reject) => {
-    exec(command, options, (error, stdout, stderr) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout);
-      }
+  // Install Dependencies
+  {
+    const {stdout, stderr} = await exec('npm --loglevel error ci --only=prod', {
+      cwd: path.resolve(__dirname)
     });
-  });
+    console.log(stdout);
+    if (stderr) {
+      return Promise.reject(stderr);
+    }
+  }
+
+  require('./src/index')();
 };
 
 run().catch(console.error);
---
+++ src/index.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core');
+const {
+  handleBranchesOption,
+  handleDryRunOption,
+  handleCiOption,
+  handleExtends,
+  handleTagFormat,
+} = require('./handleOptions');
+const setUpJob = require('./setUpJob.task');
+const installSpecifyingVersionSemantic = require('./installSpecifyingVersionSemantic.task');
+const preInstall = require('./preInstall.task');
+const cleanupNpmrc = require('./cleanupNpmrc.task');
+const windUpJob = require('./windUpJob.task');
+const inputs = require('./inputs.json');
+
+/**
+ * Release main task
+ * @returns {Promise<void>}
+ */
+const release = async () => {
+  if (core.getInput(inputs.working_directory)) {
+    process.chdir(core.getInput(inputs.working_directory));
+  }
+  await setUpJob();
+  await installSpecifyingVersionSemantic();
+  await preInstall(core.getInput(inputs.extra_plugins));
+  await preInstall(core.getInput(inputs.extends));
+
+  const semanticRelease = require('semantic-release');
+  const result = await semanticRelease({
+    ...handleBranchesOption(),
+    ...handleDryRunOption(),
+    ...handleCiOption(),
+    ...handleExtends(),
+    ...handleTagFormat()
+  });
+
+  await cleanupNpmrc();
+  await windUpJob(result);
+};
+
+module.exports = () => {
+  core.debug('Initialization successful');
+  release().catch(core.setFailed);
+};

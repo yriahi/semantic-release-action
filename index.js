9a47a66859a4d2a670b8e0133dbd9cd961560e06
Youssef Riahi <yriahi@cainc.com>
2023-05-31 22:45:19 -0400

    work in progress

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
-const { execSync } = require('child_process');
+// const { execSync } = require('child_process');
+const { exec } = require("child_process");
 const path = require('path');
 
 async function run() {
@@ -10,37 +11,46 @@ async function run() {
     const npmToken = core.getInput('NPM_TOKEN');
     
     // Install Dependencies
-    {
-      const options = {
-        cwd: path.resolve(__dirname),
-        stdio: 'pipe',
-      };
-      await executeCommand('npm ci --only=prod', options);
-    }
+    // {
+    //   const options = {
+    //     cwd: path.resolve(__dirname),
+    //     stdio: 'pipe',
+    //   };
+    //   await executeCommand('npm ci --only=prod', options);
+    // }
+    exec("npm ci --only=prod", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`Error: ${error}`);
+        return;
+      }
+    
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
     
     // Install extra plugins if specified
-    if (extraPlugins) {
-      const plugins = extraPlugins.split('\n');
-      for (const plugin of plugins) {
-        const options = {
-          cwd: path.resolve(__dirname),
-          stdio: 'pipe',
-        };
-        await executeCommand(`npm install --save-dev ${plugin}`, options);
-      }
-    }
+    // if (extraPlugins) {
+    //   const plugins = extraPlugins.split('\n');
+    //   for (const plugin of plugins) {
+    //     const options = {
+    //       cwd: path.resolve(__dirname),
+    //       stdio: 'pipe',
+    //     };
+    //     await executeCommand(`npm install --save-dev ${plugin}`, options);
+    //   }
+    // }
 
     // Perform semantic release actions
-    {
-      const options = {
-        cwd: path.resolve(__dirname),
-        stdio: 'inherit',
-      };
-      await executeCommand('npx semantic-release', options);
-    }
+    // {
+    //   const options = {
+    //     cwd: path.resolve(__dirname),
+    //     stdio: 'inherit',
+    //   };
+    //   await executeCommand('npx semantic-release', options);
+    // }
 
     // Set the outputs
-    core.setOutput('new_release_published', 'true');
+    // core.setOutput('new_release_published', 'true');
     // Set other outputs based on the release process
 
   } catch (error) {
@@ -48,15 +58,5 @@ async function run() {
   }
 }
 
-const executeCommand = async (command, options) => {
-  return new Promise((resolve, reject) => {
-    try {
-      execSync(command, options);
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
-};
 
 run().catch(console.error);
